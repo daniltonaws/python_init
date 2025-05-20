675a2137b3749605068f3cc2cfe466d942a060f2
Danilton <daniltonaws@gmail.com>
2025-05-19 22:00:33 -0300

    diferenca_att

--- exercicios/calcula_diferenca.py
+++ exercicios/calcula_diferenca.py
@@ -3,7 +3,6 @@ B = input("Digite o valor de B: ")
 C = input("Digite o valor de C: ")
 D = input("Digite o valor de D: ")
 
-print(f"Valor de A: {A}")
-print(f"Valor de B: {B}")
-print(f"Valor de C: {C}")
-print(f"Valor de D: {D}")
+diferenca = (int(A) * int(B)) - (int(C) * int(D))
+
+print(f"({A} * {B}) - ({C} * {D}) é: {diferenca}")
